e940d3e53bbdcbc60f286dcf6426648baf1d0aa3
Alexandre Frede <alexandre.frede.pro@gmail.com>
2024-10-18 17:29:22 +0200

    refactor(faire): :rotating_light: fix linting and install tailwindcss

--- projects/faire/app/routes/__root.tsx
+++ projects/faire/app/routes/__root.tsx
@@ -1,43 +1,53 @@
-import { createRootRoute } from '@tanstack/react-router'
-import { Outlet, ScrollRestoration } from '@tanstack/react-router'
-import { Body, Head, Html, Meta, Scripts } from '@tanstack/start'
-import * as React from 'react'
+import { createRootRoute } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
+import type * as React from "react";
+
+// @ts-ignore
+import appCss from "@/styles/globals.css?url";
+
+import { seo } from "@/lib/seo";
 
 export const Route = createRootRoute({
-  meta: () => [
-    {
-      charSet: 'utf-8',
-    },
-    {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1',
-    },
-    {
-      title: 'TanStack Start Starter',
-    },
-  ],
-  component: RootComponent,
-})
+	meta: () => [
+		{
+			charSet: "utf-8",
+		},
+		{
+			name: "viewport",
+			content: "width=device-width, initial-scale=1",
+		},
+		...seo({
+			title: "Faire - A Reminder App cross-platform",
+			description:
+				"Faire is a reminder app that works on all platforms. It's simple, fast, and easy to use.",
+		}),
+	],
+	links: () => [{ rel: "stylesheet", href: appCss }],
+	component: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <Html>
-      <Head>
-        <Meta />
-      </Head>
-      <Body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </Body>
-    </Html>
-  )
+	return (
+		<Html>
+			<Head>
+				<Meta />
+			</Head>
+			<Body>
+				{children}
+				<ScrollRestoration />
+				<TanStackRouterDevtools position="bottom-right" />
+				<Scripts />
+			</Body>
+		</Html>
+	);
 }
